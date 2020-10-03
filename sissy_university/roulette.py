c41a163084dfe785bb7aeb9b48863486274bcc01
Ruffiana <ruffiana.plays@gmail.com>
2020-10-03 13:33:42 -0500

    added __str__ and as_list methods

--- sissy_university/roulette.py
+++ sissy_university/roulette.py
@@ -1,19 +1,12 @@
 from copy import deepcopy
 from pprint import pprint
 
-try:
-    from .base_clases import Collection
-    from .const import DATA_ROULETTE
-except ImportError:
-    from base_clases import Collection
-    from const import DATA_ROULETTE
-
 
 
 DEFAULT_ROULETTE = {
-    "imgUrl": "#",
-    "title": "Teasing",
-    "description": "Edge 2 times. You are not allowed to cum today!"
+"imgUrl": "#",
+"title": "Teasing",
+"description": "Edge 2 times. You are not allowed to cum today!"
 }
 
 
@@ -26,17 +19,29 @@ class Slot():
         for k, v in kwargs.items():
             self.data[k] = v
 
+
+    def __str__(self):
+        return f"{self.name} - {self.description}"
+
+
+    def as_list(self):
+        return f"{self.name} - {self.description}"
+
+
     @property
     def title(self):
         return self.data.get("title")
+
 
     @property
     def name(self):
         return self.title
 
+
     @property
     def imgUrl(self):
         return self.data.get("imgUrl")
+
 
     @property
     def description(self):
@@ -44,15 +49,8 @@ class Slot():
 
 
 
-class Wheel(Collection):
-    def __init__(self, parent=None, **kwargs):
-        super().__init__(parent=parent, datafile=DATA_ROULETTE, base_class=Slot)
-
-    def __str__(self):
-        _str = [f"[{_id}] {obj.title} - {obj.description}" for _id, obj in self.collection.items()]
-        return "\n".join(_str)
-
-
 if __name__ == "__main__":
     test = Wheel()
     print(test)
+
+    
